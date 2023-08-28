f06287f526576813d29303930c3cb9f2e2c21eca
Robert018 <jbetinho31@gmail.com>
2023-08-28 16:21:04 -0300

    "mini projeto"

---
+++ teste1.py
@@ -0,0 +1,8 @@
+#crie um progama que peça um número e mostre seu sucessr e antecessor.
+#a váriavel "numero" irá pegar um número inteiro do usuário, já os outros 2(antecessor e sucessor)vão somar e subtrair,para obter os resultados.
+numero = int(input("Digite um número inteiro:"))
+antecessor = (numero - 1)
+sucessor = (numero + 1)
+#Os prits vão apresetar a menagem informando qual é o sucessor e antecessor do número escolhido.
+print("O número sucessor de:",numero,"é:",sucessor)
+print("O número antecessor de:",numero,"é:",antecessor)
